57c016bf0165165eaf5eb7787a10cd87d1a56c3c
ishbosh <99460383+ishbosh@users.noreply.github.com>
2022-11-27 13:42:39 -0700

    First attempt at computer guessing logic

--- main.rb
+++ main.rb
@@ -36,11 +36,11 @@ module MasterMind
     end
 
     def victory
-      puts show_victory(code, breaker.name)
+      puts show_victory(code, breaker.name, maker.name)
     end
 
     def game_over
-      puts show_game_over(code, breaker.name)
+      puts show_game_over(code, breaker.name, maker.name)
     end
 
     # - Methods For Setting Up The Game - #
@@ -61,9 +61,11 @@ module MasterMind
       puts show_role_prompt
       player.role = player.role_input
       if player.role == 'break'
-        @breaker = @player
+        @breaker = player
+        @maker = computer
       elsif player.role == 'make'
-        @breaker = @computer
+        @breaker = computer
+        @maker = player
       end
     end
 
@@ -89,7 +91,7 @@ module MasterMind
         
         feedback = feedback(*compare_to_code(guess))
         puts show_feedback(feedback)
-        sleep(0.5)
+        sleep(1)
       end
     end
 
@@ -191,48 +193,158 @@ module MasterMind
     end
     
     def guess(feedback)
-      update_permutations(feedback)
-      @guess = guesses == 12 ? first_guess : guess_random_permutation(feedback)
-      update_guesses_remaining
+      update_permutations(feedback) if feedback
+      @guess = guesses < 11 ? choose_guess_type(feedback) : initial_guesses
       puts show_computer_guess(@guess)
+      update_guesses_remaining
+      test_case = @possible_codes.include?(['4','5','5','6'])
       @guess
+    end
+
+    def initial_guesses
+      if guesses == 12
+        return first_guess 
+      elsif guesses == 11
+        return second_guess 
+      elsif guesses == 10
+        return third_guess 
+      end
     end
 
     def update_guesses_remaining
       self.guesses -= 1
     end
 
-    def guess_random_permutation
-      output = @possible_codes.sample.join
+    def random_permutation(feedback)
+      if feedback.join.count('x' + 'o') == 4
+        output = shuffle
+      else
+        output = @possible_codes.sample.join
+      end
     end
 
-    # def guess_using_feedback(feedback)
-    #   output = ''
-    #   update_permutations(feedback)
+    def choose_guess_type(feedback)
+      slice = feedback.join.slice(0..2)
+      best_matches(slice) if slice.count('o' + 'x') > 1
+      random_permutation(feedback)
+    end
 
+  
+    def best_matches(slice)
+      if slice.count('o') == 3
+        remove_by_triples
+      elsif slice.count('o') == 2
+        remove_by_pairs
+      elsif slice.count('o' + 'x') == 3
+        remove_by_triples_misplaced
+      else
+        remove_by_pairs_misplaced
+      end
+      test_case = @possible_codes.include?(['4','5','5','6'])
+    end
 
-    #   #   #deprecated
-    #   # feedback.each_with_index do |n, i|
-    #   #   # output[i] = @numbers.sample.to_s if n == '-'
-    #   #   # output[i] = @guess[i] if n == 'o'
-    #   #   # output[i] = @guess.each_char.map(&:to_i).sample.to_s if n == 'x'
-    #   # end
-    #   output
-    # end
+    def remove_by_triples
+      @possible_codes = @possible_codes.select do |code| 
+        code = code.join
+        code.include?(@guess[0..2]) ||
+        code.include?(@guess[1..3]) ||
+        (code.include?(@guess[0]) && code.include?(@guess[1]) && code.include?(@guess[3])) ||
+        (code.include?(@guess[0]) && code.include?(@guess[2]) && code.include?(@guess[3]))
+      end
+    end
+
+    def remove_by_triples_misplaced
+      @possible_codes = @possible_codes.select do |code| 
+        code = code.join
+        (code.include?(@guess[0]) && code.include?(@guess[1]) && code.include?(@guess[2])) ||
+        (code.include?(@guess[1]) && code.include?(@guess[2]) && code.include?(@guess[3])) ||
+        (code.include?(@guess[0]) && code.include?(@guess[1]) && code.include?(@guess[3])) ||
+        (code.include?(@guess[0]) && code.include?(@guess[2]) && code.include?(@guess[3]))
+      end
+    end
+
+    def remove_by_pairs
+      @possible_codes = @possible_codes.select do |code|
+        code = code.join
+        code.include?(@guess[0..1]) ||
+        code.include?(@guess[1..2]) ||
+        code.include?(@guess[2..3]) ||
+        (code.include?(@guess[0]) && code.include?(@guess[3])) ||
+        (code.include?(@guess[0]) && code.include?(@guess[2])) ||
+        (code.include?(@guess[1]) && code.include?(@guess[3])) 
+      end
+    end
+
+    def remove_by_pairs_misplaced
+      @possible_codes = @possible_codes.select do |code|
+        code = code.join
+        (code.include?(@guess[2]) && code.include?(@guess[3])) ||
+        (code.include?(@guess[1]) && code.include?(@guess[2])) ||
+        (code.include?(@guess[0]) && code.include?(@guess[1])) ||
+        (code.include?(@guess[0]) && code.include?(@guess[3])) ||
+        (code.include?(@guess[0]) && code.include?(@guess[2])) ||
+        (code.include?(@guess[1]) && code.include?(@guess[3])) 
+      end
+    end
 
     def update_permutations(feedback)
-      feedback_result = feedback.join == '----' ? true : false
-      # When True, remove permutations that DO include any nums from guess
-      # When False, remove permutations that do NOT include any nums from guess
-      @possible_codes = @possible_codes.select do |code| 
-        (code & @guess.chars).empty? == feedback_result
-      end
+      remove_perms_missing_guess_nums(feedback)
+
       # Remove the last incorrect guess from the possible results so no repeats
       @possible_codes.delete(@guess.chars)
+
+      @length = @possible_codes.size
+      test_case = @possible_codes.include?(['4','5','5','6'])
+    end
+
+    def shuffle(feedback)
+      output = @guess
+      loop do
+        output = @guess.shuffle
+        break if output != @guess
+      end
+      output
+    end
+
+    def remove_perms_missing_guess_nums(feedback)
+      # When last clue is x, selects perms that
+      # include ALL nums in guess AT LEAST ONCE
+      if feedback.join[-1] == 'x'
+        @possible_codes = @possible_codes.select do |code|
+          (@guess.chars - code).empty?
+        end
+      else
+        # When True, selects perms that DO NOT include ANY nums in guess
+        # When False, selects perms that include AT LEAST ONE num in guess
+        feedback_result = feedback.join.include?('----')
+        @possible_codes = @possible_codes.select do |code| 
+          (code & @guess.chars).empty? == feedback_result
+        end
+      end
     end
 
     def first_guess
-      ['1122', '2332', '4343', '6655', '4554'].sample
+      @first = [@numbers.sample * 2, @numbers.sample * 2].join
+    end
+
+    def second_guess
+      second = nil
+      loop do
+        second = [@numbers.sample * 2, @numbers.sample * 2].join
+        no_duplicates = (second.chars & @first.chars).empty?
+        break if no_duplicates
+      end
+      second
+    end
+
+    def third_guess
+      third = nil
+      loop do
+        third = [@numbers.sample * 2, @numbers.sample * 2].join
+        no_duplicates = (third.chars & second.chars & @first.chars).empty?
+        break if no_duplicates
+      end
+      third
     end
 
     def create_code
